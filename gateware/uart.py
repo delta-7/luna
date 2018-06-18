6dde7265c0c237c9270c8f48eb911291a8db97a5
Erin Moon <erin@hashbang.sh>
2018-06-18 17:08:19 -0500

    move closest_divisor to util module

--- gateware/uart.py
+++ gateware/uart.py
@@ -1,18 +1,8 @@
 from migen import *
 from migen.genlib.fsm import FSM, NextValue, NextState
 
+from .util import closest_divisor
 
-def closest_divisor(freq_base, freq_target, max_ppm=None):
-    divisor = round(freq_base / freq_target)
-
-    if divisor <= 0:
-        raise ArgumentError("Output frequency is too high")
-
-    ppm = 1000000 * ((freq_base / divisor) - freq_target) / freq_target
-    if max_ppm is not None and ppm > max_ppm:
-        raise ArgumentError("Output frequency deviation is too high ({} ppm)".format(ppm))
-
-    return divisor
 
 @ResetInserter()
 class UART(Module):
