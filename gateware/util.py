91e210cc419bdab0b5fcdf2c697b081adbf7334c
Erin Moon <erin@hashbang.sh>
2018-06-18 18:26:06 -0500

    oops, s/ArgumentError/ValueError

--- gateware/util.py
+++ gateware/util.py
@@ -2,10 +2,10 @@ def closest_divisor(freq_base, freq_target, max_ppm=None):
     divisor = round(freq_base / freq_target)
 
     if divisor <= 0:
-        raise ArgumentError("Output frequency is too high")
+        raise ValueError("Output frequency is too high")
 
     ppm = 1000000 * ((freq_base / divisor) - freq_target) / freq_target
     if max_ppm is not None and ppm > max_ppm:
-        raise ArgumentError("Output frequency deviation is too high ({} ppm)".format(ppm))
+        raise ValueError("Output frequency deviation is too high ({} ppm)".format(ppm))
 
     return divisor
