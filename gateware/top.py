6d07a985ad346f28d2e6a2bafe85006c58365c6f
Erin Moon <erin@hashbang.sh>
2018-11-17 22:41:40 -0600

    top: working restrider!

--- gateware/top.py
+++ gateware/top.py
@@ -9,28 +9,27 @@ from .restrider import Restrider
 class TopModule(Module):
     def __init__(self, plat):
         serial_pads = plat.request('serial')
+
         self.submodules.uart = UART(serial_pads, baud_rate=115200, clk_freq=12000000)
 
-        # N_PIXELS = 8
-        # fifo = SyncFIFO(24, N_PIXELS)
+        N_PIXELS = 8
+        fifo = SyncFIFO(24, N_PIXELS)
 
-        # self.submodules.restrider = Restrider()
+        self.submodules.restrider = Restrider()
 
         data = Signal(8)
-
-        self.submodules.uart_fsm = FSM(reset_state='RX')
+        self.submodules.uart_fsm = FSM()
         self.uart_fsm.act('RX',
-            self.uart.tx_ready.eq(0),
             If(self.uart.rx_ready,
                 self.uart.rx_ack.eq(1),
                 NextValue(data, self.uart.rx_data),
-                NextState('TX'),
+                NextState('INGEST'),
             )
         )
 
-        self.uart_fsm.act('TX',
-            self.uart.tx_data.eq(data),
-            self.uart.tx_ready.eq(1),
+        self.comb += self.restrider.data_in.eq(data)
+        self.uart_fsm.act('INGEST',
+            self.restrider.latch_data.eq(1),
             NextState('RX'),
         )
 
