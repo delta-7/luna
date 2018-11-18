2c0ea6343bd370be1b04cf9203a1f7d6425e3200
Erin Moon <erin@hashbang.sh>
2018-11-17 20:41:56 -0600

    top: new uart echo to proof new slurp system

--- gateware/top.py
+++ gateware/top.py
@@ -1,5 +1,6 @@
 from migen import *
 from migen.build.platforms import icestick
+from migen.genlib.fsm import FSM, NextValue, NextState
 from migen.genlib.fifo import SyncFIFO
 from .uart import UART
 from .restrider import Restrider
@@ -13,10 +14,28 @@ class TopModule(Module):
         # N_PIXELS = 8
         # fifo = SyncFIFO(24, N_PIXELS)
 
-        self.submodules.restrider = Restrider()
+        # self.submodules.restrider = Restrider()
+
+        data = Signal(8)
+
+        self.submodules.uart_fsm = FSM(reset_state='RX')
+        self.uart_fsm.act('RX',
+            self.uart.tx_ready.eq(0),
+            If(self.uart.rx_ready,
+                self.uart.rx_ack.eq(1),
+                NextValue(data, self.uart.rx_data),
+                NextState('TX'),
+            )
+        )
+
+        self.uart_fsm.act('TX',
+            self.uart.tx_data.eq(data),
+            self.uart.tx_ready.eq(1),
+            NextState('RX'),
+        )
 
 if __name__ == '__main__':
     plat = icestick.Platform()
     top = TopModule(plat)
     plat.build(top, run=True, build_dir="build")
-    # plat.create_programmer().flash(0, "build/top.bin")
+    plat.create_programmer().flash(0, "build/top.bin")
